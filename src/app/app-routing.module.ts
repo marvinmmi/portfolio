6c66c1ce6b051400f960acfe3638a3fd5571ea76
zYmMiJ <avaejimmy@gmail.com>
2020-09-13 09:23:20 +1100

    feat(AddRouting)
    Mise en place du routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { AboutComponent } from './about/about.component';
+import { AchievementComponent } from './achievement/achievement.component';
+import { ContactComponent } from './contact/contact.component';
+import { ParcoursComponent } from './parcours/parcours.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProfileCardComponent },
+  { path: 'home', component: ProfileCardComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'parcours', component: ParcoursComponent },
+  { path: 'achievement', component: AchievementComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileCardComponent } from './profile-card/profile-card.component';
 import { AboutComponent } from './about/about.component';
+import { ParcoursComponent } from './parcours/parcours.component';
+import { AchievementComponent } from './achievement/achievement.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProfileCardComponent,
-    AboutComponent
+    AboutComponent,
+    ParcoursComponent,
+    AchievementComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
